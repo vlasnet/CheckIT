873380947f417b209278ae459077da4dcf887cc8
Apelsin16 <apelsin@i.ua>
2018-06-20 16:39:23 +0300

    footer

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.css';
+
+const Footer = () => (
+    <footer styles={styles.footer}>
+        <p styles={styles.text}>Разработано при поддержке компании GoIT 2018</p>
+    </footer>    
+)
+
+export default Footer;
